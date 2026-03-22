2396797f0e0009d1f692ff811a5a7bdc05397b32
Chandan Behera <chandanbehera4598@gmail.com>
2026-03-22 15:00:08 +0530

    refernce Array add

---
+++ reference_array.java
@@ -0,0 +1,20 @@
+void main()
+{
+
+}
+
+class Customer
+{
+    private int id;
+    private String name;
+    private double bill;
+    public Customer(int id,String name,double bill)
+    {
+        this.id=id;
+        this.name=name;
+        this.bill=bill;
+
+    }
+    
+
+}
