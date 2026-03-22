204c8b98657f81d5af59685d63d1be6cfd919571
Chandan Behera <chandanbehera4598@gmail.com>
2026-03-22 14:58:33 +0530

    Add files via upload

---
+++ duplicates.java
@@ -0,0 +1,45 @@
+//!Remove Duplicates
+
+// void main()
+// {
+//     int arr[]={10,20,10};
+
+//     for(int i=0;i<arr.length;i++)
+//     {
+//         for(int j=i+1;j<arr.length;j++)
+//         {
+//             if(arr[i]==arr[j])
+//             {
+//                 arr[j]=-1;
+
+//             }
+//         }
+//     }
+   
+//     for(int j=0;j<arr.length;j++)
+//     {
+// if(arr[j]!=-1)
+// {
+// IO.println(arr[j]+" ");
+// }
+//     }
+// }
+
+//!Duplicate
+
+// void main()
+// {
+//     int arr[]={10,20,30,10};
+//     for(int i=0;i<arr.length;i++)
+//     {
+//         for(int j=i+1;j<arr.length;j++)
+//         {
+// if(arr[i]==arr[j])
+// {
+// IO.println(arr[j]);
+// break;
+
+// }
+//         }
+//     }
+// }
